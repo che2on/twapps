a96b4024c42141cc8359006a8812ffc533394107
chethan <che4on@gmail.com>
2014-03-19 23:46:23 +0530

    tv  port number from 3368 to 2368

--- tv/app.js
+++ tv/app.js
@@ -13,17 +13,17 @@ var apiProxy = httpProxy.createProxyServer();
 
 app.get("/*", function(req, res, next){ 
   console.log("proxy starts")  ;
-  apiProxy.web(req, res, { target: 'http://tv.tweetaly.st:2368' });
+  apiProxy.web(req, res, { target: 'http://tv.tweetaly.st:3368' });
   console.log("proxy ends")  ;
 });
 app.post("/*", function(req, res, next){ 
-  apiProxy.web(req, res, { target: 'http://tv.tweetaly.st:2368' });
+  apiProxy.web(req, res, { target: 'http://tv.tweetaly.st:3368' });
 });
 app.delete("/*", function(req, res, next){ 
-  apiProxy.web(req, res, { target: 'http://tv.tweetaly.st:2368' });
+  apiProxy.web(req, res, { target: 'http://tv.tweetaly.st:3368' });
 });
 app.put('/*', function(req, res, next) {
-        apiProxy.web(req, res, { target: 'http://tv.tweetaly.st:2368'});
+        apiProxy.web(req, res, { target: 'http://tv.tweetaly.st:3368'});
     });
 
 app.use(express.urlencoded());
